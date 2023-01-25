d424ae93feccb1ac65183fe900de4fb62d3387a4
rebecca <rebeccaymchang@gmail.com>
2023-01-24 19:00:32 -0500

    Don't touch the cubes 6
    Scripts and cube prefab are done.

--- A.S - Don't Touch The Cubes/Assets/Scripts/SpawnObjects.cs
+++ A.S - Don't Touch The Cubes/Assets/Scripts/SpawnObjects.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class SpawnObjects : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
+    [Header("Spawn Cube Object")]
+    public GameObject spawnCube;
+    [Header("Default Difficulty")]
+    public float diffculty = 40f;
+    float spawn;
+    
     void Update()
     {
-
+        spawn = diffculty * Time.deltaTime;
+        diffculty = Time.deltaTime * 4f;
+        while (spawn > 0)
+        {
+            spawn -= 1;
+            Vector3 v3Pos = transform.position + new Vector3(Random.value * 40f - 20f, 0, Random.value * 40f - 20f);
+            Quaternion qRotation = Quaternion.Euler(0, Random.value * 360f, Random.value * 30f);
+            GameObject createObject = Instantiate(spawnCube, v3Pos, qRotation);
+        }
     }
 }
