86e8b85c314143fd10950cbe45459207bf791cd2
Aroosh <aroosh.manha@gmail.com>
2023-04-15 14:00:01 -0400

    Dropping bombs pt3 42

--- AS - My Dropping Bombs 2/Assets/Spawner.cs
+++ AS - My Dropping Bombs 2/Assets/Spawner.cs
@@ -6,11 +6,12 @@ public class Spawner : MonoBehaviour
 {
     public GameObject bombPrefab;
     public float delay = 2.0f;
-    publiv active = true;
+    public bool active = true;
     public Vector2 delayRange = new Vector2(1, 2);
-    private Vector2 screenBounds;
-    private flat objectWidth
 
+    private Vector2 screenBounds;
+    private float objectWidth;
+    private float objectHeight;
 
 
 
@@ -19,6 +20,10 @@ public class Spawner : MonoBehaviour
     {
         ResetDelay();
         StartCoroutine(EnemyGenerator());
+
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        objectWidth = bombPrefab.GetComponent<MeshRenderer>().bounds.size.x / 2;
+        objectHeight = bombPrefab.GetComponent<MeshRenderer>().bounds.size.y / 2;
     }
 
     IEnumerator EnemyGenerator()
@@ -26,8 +31,11 @@ public class Spawner : MonoBehaviour
         yield return new WaitForSeconds(delay);
         if (active)
         {
+            float randomX = Random.Range(screenBounds.x - objectWidth, screenBounds.x * -1 - objectWidth);
+            float spawnY = (screenBounds.y + objectHeight) + 5;
+
             Instantiate(bombPrefab, new Vector3(randomX, spawnY, 0), bombPrefab.transform.rotation);
-            ReserDelay();
+            ResetDelay();
         }
         StartCoroutine(EnemyGenerator());
     }
