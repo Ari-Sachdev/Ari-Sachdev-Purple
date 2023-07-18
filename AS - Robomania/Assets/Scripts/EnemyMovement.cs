b1a784196d5cd08bfd5aeab34fd3ce7df71d58a4
Aroosh <markhamonca@codeninjas.com>
2023-07-18 18:59:48 -0400

    PY robo doot doot

--- AS - Robomania/Assets/Scripts/EnemyMovement.cs
+++ AS - Robomania/Assets/Scripts/EnemyMovement.cs
@@ -7,20 +7,31 @@ public class EnemyMovement : MonoBehaviour
 
     public float yForce;
     public float xForce1;
+    public float xForce2;
     public float xForce;
     public float xDirection;
     private Rigidbody2D enemyRigidBody;
-
+    
+    void EnemySpeed()
+    {
+        xForce = xForce2;
+        xForce *= 1.5f;
+        if (xForce > 1000)
+        {
+            xForce = xForce1;
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
         enemyRigidBody = GetComponent<Rigidbody2D>();
-        xForce = xForce1;
+        xForce1 = xForce;
+        xForce2 = xForce;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Ground")
+        if (collision.gameObject.tag == "Ground" || collision.gameObject.tag == "Ground")
         {
             Vector2 jumpForce = new Vector2(xForce * xDirection, yForce);
             enemyRigidBody.AddForce(jumpForce);
@@ -32,13 +43,13 @@ public class EnemyMovement : MonoBehaviour
     {
         if (collision.gameObject.tag == "Bullet")
         {
-            xForce *= 1.5f;
-            if (xForce > 1000)
-            {
-                xForce = xForce1;
-            }
+            xForce2 = xForce;
+            xForce = 0;
+            Invoke("EnemySpeed", 0.2f);
         }
     }
+
+
     // Update is called once per frame
     private void FixedUpdate()
     {
